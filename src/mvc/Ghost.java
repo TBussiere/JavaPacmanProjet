1e81ee4904dfa8a87919b4b3c62666a42ab3d69c
DARGIER DE ST VAULRY JEAN-BAPTISTE p1601788 <jean-baptiste.dargier-de-st-vaulry@etu.univ-lyon1.fr>
2018-11-14 15:31:44 +0100

    Ajout Pacman et Ghost

---
+++ src/mvc/Ghost.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvc;
+
+/**
+ *
+ * @author Jean-Baptiste
+ */
+public class Ghost extends Entite{
+    private String color;
+    
+    public Ghost(){
+        this.color="";
+    }
+    
+    @Override
+    public void realiserAction(){
+        
+    }
+}
---
+++ src/mvc/Pacman.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvc;
+
+/**
+ *
+ * @author Jean-Baptiste
+ */
+
+public class Pacman extends Entite {
+    private int score;
+    
+    public Pacman(){
+        this.score = 0;
+    }
+    
+    public String getCurrentDirection(){
+        this.currentDirection = "";
+        return this.currentDirection;
+    }
+    
+    @Override
+    public void realiserAction(){
+
+    }
+    
+    public void moveUp(){
+        
+    }
+    
+    public void moveDown(){
+        
+    }
+    
+    public void moveLeft(){
+        
+    }
+    
+    public void moveRight(){
+        
+    }
+}
