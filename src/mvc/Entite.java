13d6238d87faa1fe342a68f2ce2db695b8f7c8cc
DARGIER DE ST VAULRY JEAN-BAPTISTE p1601788 <jean-baptiste.dargier-de-st-vaulry@etu.univ-lyon1.fr>
2018-11-14 15:17:36 +0100

    Ajout Entite.java

---
+++ src/mvc/Entite.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvc;
+
+/**
+ *
+ * @author Jean-Baptiste
+ */
+public class Entite implements Runnable{
+    protected String type;
+    protected String currentDirection;
+
+    public String getCurrentDirection() {
+        return currentDirection;
+    }
+
+    public void setCurrentDirection(String currentDirection) {
+        this.currentDirection = currentDirection;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
