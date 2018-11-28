10a2e21e950a6ce7503243b230e0b924472f86ac
DARGIER DE ST VAULRY JEAN-BAPTISTE p1601788 <jean-baptiste.dargier-de-st-vaulry@etu.univ-lyon1.fr>
2018-11-28 17:33:06 +0100

    Changement couleur pacman mangeable

--- src/mvc/Pacman.java
+++ src/mvc/Pacman.java
@@ -14,7 +14,7 @@ public class Pacman extends Entite {
     public int score;
     public boolean superPacman;
     public int curDurationSuperPacmen;
-    public int baseDurationSuperPacmen = 200;
+    public int baseDurationSuperPacmen = 80;
     
     
 
